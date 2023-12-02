2b53c482ab0dc1081e8633228345e170c1224ac0
Patrick James <120973196+CUagiain@users.noreply.github.com>
2023-12-03 04:47:15 +0800

    Update change.js

--- change.js
+++ change.js
@@ -10,13 +10,13 @@ for (let i = 0; i < info_btn.length; i++) {
         var userType = document.getElementById('userType').value;
         switch (userType) {
             case 'Seller':
-                window.location.href = 'https://cuagiain.github.io/Group11/creatShop.html'; // 替换为卖家页面的
+                window.location.href = 'https://cuagiain.github.io/Group11/creatShop.html'; 
                 break;
-            case 'User':
-                window.location.href = 'https://cuagiain.github.io/Group11/home.html'; // 替换为老年人页面
+            case 'Buyer':
+                window.location.href = 'https://cuagiain.github.io/Group11/home.html'; 
                 break;
             case 'Volunteer':
-                window.location.href = 'https://cuagiain.github.io/Group11/info.html'; // 替换为志愿者页面
+                window.location.href = 'https://cuagiain.github.io/Group11/info.html';
                 break;
             default:
                 
